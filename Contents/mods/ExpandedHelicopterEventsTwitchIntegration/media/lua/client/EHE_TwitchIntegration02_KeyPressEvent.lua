badeb7d541021eeddd0b1a1ce993d74b00358c20
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-11-14 20:38:14 -0500

    Update EHE_TwitchIntegration02_KeyPressEvent.lua

--- Contents/mods/ExpandedHelicopterEventsTwitchIntegration/media/lua/client/EHE_TwitchIntegration02_KeyPressEvent.lua
+++ Contents/mods/ExpandedHelicopterEventsTwitchIntegration/media/lua/client/EHE_TwitchIntegration02_KeyPressEvent.lua
@@ -10,24 +10,24 @@ function twitchIntegration_OnKeyPressed(key)
 		if (not isAdmin() and not isCoopHost() and not getDebug()) then
 			return
 		end
+	end
 
-		local twitchKey = twitchKeys[Keyboard.getKeyName(key)]
-		if twitchKey then
+	local twitchKey = twitchKeys[Keyboard.getKeyName(key)]
+	if twitchKey then
 
-			local players = getActualPlayers()
-			---@type IsoGameCharacter|IsoPlayer|IsoMovingObject|IsoObject
-			local playerChar = players[ZombRand(#players)+1]
+		local players = getActualPlayers()
+		---@type IsoGameCharacter|IsoPlayer|IsoMovingObject|IsoObject
+		local playerChar = players[ZombRand(#players)+1]
 
-			if eHelicopterSandbox.config.twitchStreamerTargeted == true then playerChar = getPlayer() end
-			if playerChar then
-				local numpadKey = eHelicopterSandbox.config[twitchKey]
-				--local presetID = twitchIntegrationPresets[numpadKey]
-				--if presetID=="RANDOM" then presetID = twitchIntegrationPresets[ZombRand(2,#twitchIntegrationPresets)] end
-				print("-- scheduleEvent: twitchKey: "..tostring(twitchKey))--.." numpadKey:"..tostring(numpadKey)).." presetID:"..tostring(presetID))
-				--if presetID=="NONE" then return end
-				local pUsername = playerChar:getUsername()
-				sendClientCommand("twitchIntegration", "scheduleEvent", {twitchKey=twitchKey,numpadKey=numpadKey,twitchTarget=pUsername})
-			end
+		if eHelicopterSandbox.config.twitchStreamerTargeted == true then playerChar = getPlayer() end
+		if playerChar then
+			local numpadKey = eHelicopterSandbox.config[twitchKey]
+			--local presetID = twitchIntegrationPresets[numpadKey]
+			--if presetID=="RANDOM" then presetID = twitchIntegrationPresets[ZombRand(2,#twitchIntegrationPresets)] end
+			print("-- scheduleEvent: twitchKey: "..tostring(twitchKey))--.." numpadKey:"..tostring(numpadKey)).." presetID:"..tostring(presetID))
+			--if presetID=="NONE" then return end
+			local pUsername = playerChar:getUsername()
+			sendClientCommand("twitchIntegration", "scheduleEvent", {twitchKey=twitchKey,numpadKey=numpadKey,twitchTarget=pUsername})
 		end
 	end
 end
